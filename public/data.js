e8b3ab5fa88c3a2f90dbca07f1aab144e6ff1f7b
Kaikiat <kaikiat@gmail.com>
2022-11-24 02:27:44 +0800

    feat: add tool

---
+++ public/data.js
@@ -0,0 +1,41 @@
+let questionData = {
+    "q1": {
+        "question": "Name a reason a coworker might give you a high five",
+        "answer": {
+            "1": "Show Encouragement",
+            "2": "Nailed a Presentation",
+            "3": "Had a Good Idea",
+            "4": "Show Solidarity",
+            "5": "Just Say Hi",
+            "6": "Show Support",
+            "7": "Show Enthusiasm",
+            "8": "Show Respect",
+        }
+    },
+    "q2": {
+        "question": "Name a culture that could improve colleages relationship",
+        "answer": {
+            "1": "Gathering Culture",
+            "2": "Napping Culture",
+            "3": "Chatting Culture",
+            "4": "Lazy Work culture",
+            "5": "Entertainment culture",
+            "6": "Ponteng culture",
+            "7": "LGBT culture",
+            "8": "Food culture",
+        }
+    },
+    "q3": {
+        "question": "Name somewhere in an office you might run into a colleague and stop for a chat",
+        "answer": {
+            "1": "Break room",
+            "2": "Washroom",
+            "3": "Copy Room",
+            "4": "Water Cooler",
+            "5": "Hallway",
+            "6": "Elevator",
+            "7": "Workstation",
+            "8": "Front Desk",
+        }
+    }
+}
